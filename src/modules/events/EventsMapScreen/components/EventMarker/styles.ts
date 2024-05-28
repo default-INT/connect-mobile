051d7c11a748225466080208fa6701ba66a97ad3
Yauheni Trafimau <yauheni.trafimau@itechart-group.com>
2024-05-28 22:17:22 +0300

    #42 Fix marker container size on Android

--- src/modules/events/EventsMapScreen/components/EventMarker/styles.ts
+++ src/modules/events/EventsMapScreen/components/EventMarker/styles.ts
@@ -1,21 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import { theme } from '@root/styles/theme';
 import { s } from '@utils/scaleUtils/scale';
 
+// NOTE: https://github.com/react-native-maps/react-native-maps/issues/5068
+const containerSize = Platform.OS === 'ios' ? s.width(54) : s.width(24);
+const iconPadding = Platform.OS === 'ios' ? s.max(8) : s.width(2);
+
 export const styles = StyleSheet.create({
   container: {
-    width: s.width(54),
-    height: s.height(54),
-    backgroundColor: theme.mainBasic,
-    borderBlockColor: theme.mainExtra,
-    borderWidth: s.width(1),
-    borderRadius: s.max(54) / 2,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   iconContainer: {
-    width: s.width(42),
-    height: s.height(42),
+    width: containerSize,
+    height: containerSize,
+    backgroundColor: theme.mainBasic,
+    borderBlockColor: theme.mainExtra,
+    borderWidth: s.width(1),
+    borderRadius: containerSize / 2,
+    padding: iconPadding,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
