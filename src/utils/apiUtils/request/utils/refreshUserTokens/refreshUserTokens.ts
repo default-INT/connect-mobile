b56228ac38d55a255b6ac3082800fc2cdcc60091
Yauheni Trafimau <yauheni.trafimau@itechart-group.com>
2024-06-02 15:47:41 +0300

    #52 Improve auth logic

--- src/utils/apiUtils/request/utils/refreshUserTokens/refreshUserTokens.ts
+++ src/utils/apiUtils/request/utils/refreshUserTokens/refreshUserTokens.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import { IUserTokens } from '@root/types/user';
+import { storage } from '@utils/storage';
+import { env } from '@constants/env';
 
-const REFRESH_TOKEN_URL = '/api/auth/token/refresh';
+const REFRESH_TOKEN_URL = `${env.API_URL}/api/auth/token/refresh`;
 
 export const refreshUserTokens = async () => {
   try {
+    const data = await storage.getTokens();
+
     const response = await axios.request<IUserTokens>({
       url: REFRESH_TOKEN_URL,
       method: 'POST',
+      data,
     });
 
     return response.data;
